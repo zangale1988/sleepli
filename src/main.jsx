55078234a5f1750e4b5e765ca5f66befb6b960a0
zangale1988 <alexander.zangerl@gmail.com>
2025-07-29 09:31:05 +0200

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import SleepliApp from "./SleepliApp";
-import "./style.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <SleepliApp />
-  </React.StrictMode>
-);
+console.log('Main entry');
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,1 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()]
-})
+export default {};
