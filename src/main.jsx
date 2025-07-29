10963755cefed3c32f3a0326362ab71dfc9e6c0b
zangale1988 <alexander.zangerl@gmail.com>
2025-07-29 08:41:19 +0200

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SleepliApp from "./SleepliApp";
+import "./style.css";
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import SleepliApp from './SleepliApp.jsx'
-import './style.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <SleepliApp />
   </React.StrictMode>
-)
+);
